1e38a510ba9a988e0cc9b782020b2c6b680cc3e5
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-07-27 01:58:35 +0000

    [Finish] Javascript２章

---
+++ app.js
@@ -0,0 +1,83 @@
+//alert('Hello World');
+
+//変数
+//let hello = 'Hello World';
+//alert(hello);
+
+//整数を代入
+//let int1 = 1;
+//alert(int1);
+
+//負数を代入
+//let int2 = -10;
+//alert(int2);
+
+//小数点を代入
+//let float1 = 3.14;
+//alert(float1);
+
+//文字列を代入
+//let str1 = 'JavaScriptを覚えよう';
+//alert(str1);
+
+//足し算
+//alert(4 + 3);
+
+//引き算
+//alert(8 - 5);
+
+//掛け算
+//alert(2 * 6);
+
+//割り算
+//alert(10 / 2);
+
+//文字列結合
+//alert('Hello' + 'World');
+
+//let str1 = 'Hello';
+//let str2 = 'World';
+//alert(str1 + str2);
+
+//条件分岐
+//let orange = 100;
+//let apple = 120;
+
+//if(orange < apple){
+//  alert('みかんの値段がりんごより安い');
+//}else if(orange == apple){
+//  alert('みかんとりんごの同じ値段');
+//}else{
+//  alert('みかんの値段がりんごより高い');
+//}
+
+//繰り返し処理
+//let max = 100;
+//let num = 1;
+//let count = 0;
+
+//while(num < max){
+  //num = num *2;
+  //count = count + 1;
+//}
+
+//alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+//let i;
+//let num = 0;
+//for(i=1; i<11; i++){
+//  num = num + i;
+//}
+//alert('1から10まで足し算した結果は' + num +'です')
+
+
+//確認問題
+//let num1 = (4+6);
+//let num2 = (4-6);
+//let num3 = (4*6);
+//let num4 = (4/6);
+
+//alert('4+6='+num1);
+//alert('4-6='+num2);
+//alert('4*6='+num3);
+//alert('4/6='+num4);
